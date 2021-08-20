fba01776b48a233dd337d9cce6722354e6d7d16b
Kaunain Ahmad <kaunain.ahmad2000@gmail.com>
2021-08-20 12:34:55 +0530

    page optimizaation

--- quotes/seo.py
+++ quotes/seo.py
@@ -9,36 +9,37 @@ qWidth = int(env.get('quotes', 'WIDTH'))
 
 def setMetas(qots, url):
     desc = seoDec
-    metas = {}
+    metaProps = {}
+    metaNames = {}
     if qots:
         qot = qots[0]
         imgPath = imgPrefix + imgDir + qot.imagePath
         title = qot.title
         title = title if title else qot.imageAlt
-        metas['title'] = qot.imageAlt
-        metas['og:site_name'] = 'BestRani'
-        metas['og:title'] = qot.imageAlt
-        metas['og:type'] = 'article'
+        metaNames['title'] = qot.imageAlt
+        metaProps['og:site_name'] = 'BestRani'
+        metaProps['og:title'] = qot.imageAlt
+        metaProps['og:type'] = 'article'
         desc = qot.quotes
         desc = qot.desc if qot.desc else desc
-        metas['description'] = desc
-        metas['og:description'] = desc
-        metas['og:url'] = url
-        metas['og:image'] = imgPath
-        metas['og:image:type'] = 'image/jpeg'
-        metas['og:image:width'] = qWidth
-        metas['og:image:height'] = qHeight
-        metas['og:locale'] = 'en_US'
-        metas['og:locale:alternate'] = 'en_IN'    
-        metas['twitter:card'] = 'summary'       
-        metas['twitter:site'] = '@BestRani3'       
-        metas['twitter:url'] = url      
-        metas['twitter:title'] = qot.imageAlt     
-        metas['twitter:description'] = desc     
-        metas['twitter:image'] = imgPath
-        metas['datePublished'] = qot.publishAt.strftime("%Y-%m-%dT%H:%M:%S%z")
-        metas['article:published_time'] = qot.publishAt.strftime("%Y-%m-%dT%H:%M:%S%z")
-        metas['article:author'] = 'Iffat Zia'
+        metaNames['description'] = desc
+        metaProps['og:description'] = desc
+        metaProps['og:url'] = url
+        metaProps['og:image'] = imgPath
+        metaProps['og:image:type'] = 'image/jpeg'
+        metaProps['og:image:width'] = qWidth
+        metaProps['og:image:height'] = qHeight
+        metaProps['og:locale'] = 'en_US'
+        metaProps['og:locale:alternate'] = 'en_IN'
+        metaNames['twitter:card'] = 'summary'
+        metaNames['twitter:site'] = '@BestRani3'
+        metaNames['twitter:url'] = url
+        metaNames['twitter:title'] = qot.imageAlt
+        metaNames['twitter:description'] = desc
+        metaNames['twitter:image'] = imgPath
+        metaNames['datePublished'] = qot.publishAt.strftime("%Y-%m-%dT%H:%M:%S%z")
+        metaNames['article:published_time'] = qot.publishAt.strftime("%Y-%m-%dT%H:%M:%S%z")
+        metaNames['article:author'] = 'Iffat Zia'
         #if qot.updatedAt > qot.publishAt:
         #    metas['dateModified'] = qot.publishAt.strftime("%Y-%m-%dT%H:%M:%S%z")
-    return metas
+    return {'metaNames': metaNames, 'metaProps': metaProps}
